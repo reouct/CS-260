120151c045a6a960a5aac9e49cea3c6a8e1c49e6
Bowen Liu <Bowen113355@byui.edu>
2023-11-18 21:06:32 -0700

    Update database.js

---
+++ database.js
@@ -0,0 +1,25 @@
+const { MongoClient } = require("mongodb");
+const config = require("./dbConfig.json");
+
+const url = `mongodb+srv://${config.userName}:${config.password}@${config.hostname}`;
+const client = new MongoClient(url);
+const db = client.db("answers");
+const answerCollection = db.collection("answer");
+
+// This will asynchronously test the connection and exit the process if it fails
+(async function testConnection() {
+  await client.connect();
+  await db.command({ ping: 1 });
+})().catch((ex) => {
+  console.log(
+    `Unable to connect to database with ${url} because ${ex.message}`
+  );
+  process.exit(1);
+});
+
+async function getanwser(answer) {
+  const result = await answerCollectionC.add(answer);
+  return result;
+}
+
+module.exports = { getanwser };
