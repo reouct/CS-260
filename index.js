4f804f9d21804c23cea716cb47f478463f7cc37e
Bowen Liu <Bowen113355@byui.edu>
2023-12-02 15:12:30 -0700

    Update Websocket Servcie page

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 // Serve up the applications static content
-app.use(express.static("public"));
+app.use(express.static("./public"));
 
 // Trust headers that are forwarded from the proxy so we can determine IP addresses
 app.set("trust proxy", true);
@@ -117,7 +117,64 @@ function setAuthCookie(res, authToken) {
     sameSite: "strict",
   });
 }
+/*
+// Websocket server
+const WebSocket = require("ws");
+// Create a websocket object
+const wss = new WebSocketServer({ noServer: true });
 
+// Handle the protocol upgrade from HTTP to WebSocket
+server.on("upgrade", (request, socket, head) => {
+  wss.handleUpgrade(request, socket, head, function done(ws) {
+    wss.emit("connection", ws, request);
+  });
+});
+
+// Keep track of all the connections so we can forward messages
+let connections = [];
+
+wss.on("connection", (ws) => {
+  const connection = { id: connections.length + 1, alive: true, ws: ws };
+  connections.push(connection);
+
+  // Forward messages to everyone except the sender
+  ws.on("message", function message(data) {
+    connections.forEach((c) => {
+      if (c.id !== connection.id) {
+        c.ws.send(data);
+      }
+    });
+  });
+
+  // Remove the closed connection so we don't try to forward anymore
+  ws.on("close", () => {
+    connections.findIndex((o, i) => {
+      if (o.id === connection.id) {
+        connections.splice(i, 1);
+        return true;
+      }
+    });
+  });
+
+  // Respond to pong messages by marking the connection alive
+  ws.on("pong", () => {
+    connection.alive = true;
+  });
+});
+
+// Keep active connections alive
+setInterval(() => {
+  connections.forEach((c) => {
+    // Kill any connection that didn't respond to the ping last time
+    if (!c.alive) {
+      c.ws.terminate();
+    } else {
+      c.alive = false;
+      c.ws.ping();
+    }
+  });
+}, 10000);
+*/
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
 });
