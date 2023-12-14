ff2bcbe517b6993c66516a18da6b6c1be6f47c97
Bowen Liu <Bowen113355@byui.edu>
2023-12-13 21:48:10 -0700

    Finish creating the router

---
+++ src/service/service.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export function Service() {
+  return (
+    <main className="container-fluid bg-secondary text-center">
+      <div>service displayed here</div>
+    </main>
+  );
+}
